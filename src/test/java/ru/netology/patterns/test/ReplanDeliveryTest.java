71aceb656ab2bce3bf087ecfe934ea8a579b689c
Александр Саломанин <alexzsanjes@gmail.com>
2026-04-10 22:02:29 +0700

    added report

--- src/test/java/ru/netology/patterns/test/ReplanDeliveryTest.java
+++ src/test/java/ru/netology/patterns/test/ReplanDeliveryTest.java
@@ -1,7 +1,13 @@
 package ru.netology.patterns.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +25,17 @@ public class ReplanDeliveryTest {
 
     @BeforeAll
     static void setupAll() {
-//        Configuration.browser = "firefox";
-//        Configuration.holdBrowserOpen = true;
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.browser = "firefox";
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
+
+    @AfterAll
+    static void tearDown() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
@@ -32,23 +45,35 @@ public class ReplanDeliveryTest {
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         SelenideElement form = $("fieldset");
-        form.$("[data-test-id=city] input").setValue(validUser.getCity());
-        form.$("[data-test-id=date] input").
-                press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE).setValue(firstMeetingDate);
-        form.$("[data-test-id=name] input").setValue(validUser.getName());
-        form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
-        form.$("[data-test-id=agreement]").click();
-        form.$$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id=success-notification] .notification__content").
-                shouldBe(visible, Duration.ofSeconds(15)).
-                shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
-        form.$("[data-test-id=date] input").
-                press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE).setValue(secondMeetingDate);
-        form.$$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id=replan-notification] .button__content .button__text").
-                shouldBe(visible).click();
-        $("[data-test-id=success-notification] .notification__content").
-                shouldBe(visible, Duration.ofSeconds(15)).
-                shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
+        Allure.step("Выбор места и времени", () -> {
+            form.$("[data-test-id=city] input").setValue(validUser.getCity());
+            form.$("[data-test-id=date] input").
+                    press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE).setValue(firstMeetingDate);
+        });
+        Allure.step("Заполнение контактных данных", () -> {
+            form.$("[data-test-id=name] input").setValue(validUser.getName());
+            form.$("[data-test-id=phone] input").setValue(validUser.getPhone());
+        });
+        Allure.step("Соглашение с условиями и планирование", () -> {
+            form.$("[data-test-id=agreement]").click();
+            form.$$("button").find(exactText("Запланировать")).click();
+        });
+        Allure.step("Уведовление об успешном планировнаии", () -> {
+            $("[data-test-id=success-notification] .notification__content").
+                    shouldBe(visible, Duration.ofSeconds(15)).
+                    shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        });
+        Allure.step("Перепланирование даты", () -> {
+            form.$("[data-test-id=date] input").
+                    press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE).setValue(secondMeetingDate);
+            form.$$("button").find(exactText("Запланировать")).click();
+            $("[data-test-id=replan-notification] .button__content .button__text").
+                    shouldBe(visible).click();
+        });
+        Allure.step("Уведомление об успешном перепланировании", () -> {
+            $("[data-test-id=success-notification] .notification__content").
+                    shouldBe(visible, Duration.ofSeconds(15)).
+                    shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
+        });
     }
 }
